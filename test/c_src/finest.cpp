c244a9140c8db33c733aaccbae865cd381f51dbc
Alexis Brodeur <brodeuralexis@gmail.com>
2025-06-04 22:28:00 -0400

    Provide templated and polymorphic allocators
    This commits provides a templated allocator for STL containers and a
    memory resource for `std::pmr::polymorphic_allocator`.
    
    This enables the use of `enif_alloc` and `enif_free` from all STL
    containers while leave the user the choice to use templates or
    polymorphism to select an allocator.

--- test/c_src/finest.cpp
+++ test/c_src/finest.cpp
@@ -295,6 +295,21 @@ std::nullopt_t shared_mutex_shared_lock_test(ErlNifEnv *) {
 }
 FINE_NIF(shared_mutex_shared_lock_test, 0);
 
+std::vector<std::pmr::string, fine::Allocator<std::pmr::string>> allocators(
+    ErlNifEnv *,
+    std::basic_string<char, std::char_traits<char>, fine::Allocator<char>>
+        string,
+    std::uint64_t repeat) {
+  std::vector<std::pmr::string, fine::Allocator<std::pmr::string>> strings;
+
+  for (std::uint64_t i = 0; i != repeat; ++i) {
+    strings.emplace_back(std::pmr::string(string, fine::memory_resource));
+  }
+
+  return strings;
+}
+FINE_NIF(allocators, 0);
+
 } // namespace finest
 
 FINE_INIT("Elixir.Finest.NIF");
